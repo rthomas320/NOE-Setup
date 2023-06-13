044222cfe792c2c0a8832c562f2c94efa6a505cc
rt320 <98881959+rt320@users.noreply.github.com>
2023-06-13 08:57:24 -0400

    Modifications to enable nar file build Add mock analyzer and sanitzier scripts Modifications to provisioning Convert CR/LF to LF

---
+++ sanitizer2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIRTOMONITOR="/home/nifi/data/mockSanitizer2/in"
+DIRTOWRITE="/home/nifi/data/mockSanitizer2/out"
+  while true
+  do
+    ls $DIRTOMONITOR | while read NEWFILE
+    do
+      CHECK=${#NEWFILE}
+      if [ $CHECK -ge 1 ]
+      then
+        echo "The file '$NEWFILE' was modified and moved to out"
+        echo "The file '$NEWFILE' was modified and moved to out" >> /home/nifi/data/mockSanitizer1/sanitzer1.log
+        echo 's2' >> $DIRTOMONITOR/$NEWFILE
+	echo "Moving '$DIRTOMONITOR'/'$NEWFILE' to '$DIRTOWRITE'/'$NEWFILE'"
+        mv $DIRTOMONITOR/$NEWFILE $DIRTOWRITE/$NEWFILE
+      fi
+    done
+    sleep 5
+  done
+
