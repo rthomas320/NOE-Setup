8d25b00edbca031b318c1e3517fe66803b5b006a
regis.thomas <regis.thomas@nteligen.com>
2023-06-14 07:18:27 -0400

    Modify java-home script

--- provisioning/roles/environment/files/java-home.sh
+++ provisioning/roles/environment/files/java-home.sh
@@ -1,3 +1,6 @@
 export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+echo 'Set JAVA_HOME='$JAVA_HOME
 export PATH=$PATH:$JAVA_HOME/bin
-export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+echo 'Set PATH='$PATH
+export CLASSPATH=.:$JAVA_HOME/lib
+echo 'Set CLASSPATH='$CLASSPATH
