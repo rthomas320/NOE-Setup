644714b76dac5c9863ea7a5db5f7da4f7700fdf8
regis.thomas <regis.thomas@nteligen.com>
2023-06-22 09:49:32 -0400

    Final NOE provisioning

---
+++ provisioning/roles/environment/files/start-nifi-mock-processors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+/home/nifi/data/analyzer1.sh &
+#/home/nifi/data/analyzer2.sh &
+#/home/nifi/data/analyzer3.sh &
+/home/nifi/data/sanitizer1.sh &
+/home/nifi/data/sanitizer2.sh &
+/home/nifi/data/sanitizer3.sh &
+
